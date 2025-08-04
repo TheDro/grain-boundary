f7a5d299a22b87ece72a16c39a16d6986f604c0b
TheDro <andrewsc32@protonmail.com>
2025-08-03 20:09:22 -0400

    Added delay endpoints

---
+++ app/controllers/delay_controller.rb
@@ -0,0 +1,30 @@
+class DelayController < ApplicationController
+
+  def index
+    sleep_time
+    run_time
+
+    current_port = request.port
+
+    render json: { 
+      current_port: current_port,
+      status: "ok" 
+    }
+  end
+
+  def sleep_time
+    time = params[:sleep_time].to_f
+
+    if time > 0
+      sleep(time)
+    end
+  end
+
+  def run_time
+    time = params[:run_time].to_f
+
+    if time > 0
+      raise "Not implemented yet"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "a" => "delay#index"
+  get "b" => "delay#index"
+  get "c" => "delay#index"
+  get "d" => "delay#index"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
